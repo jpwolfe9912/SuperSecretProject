93d0623da0e38e616d5ee079a2b7ecb09ffecd73
Jeremy Wolfe <jpwolfe@me.com>
2023-03-31 21:34:01 -0700

    No real changes. Considered changing priorities

--- Core/Src/dma.c
+++ Core/Src/dma.c
@@ -45,22 +45,22 @@ void MX_DMA_Init(void)
 
     /* DMA interrupt init */
     /* DMA1_Channel2_IRQn interrupt configuration */
-    NVIC_SetPriority(DMA1_Channel2_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0, 0));
+    NVIC_SetPriority(DMA1_Channel2_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0, 0));// SPI RX
     NVIC_EnableIRQ(DMA1_Channel2_IRQn);
     /* DMA1_Channel3_IRQn interrupt configuration */
-    NVIC_SetPriority(DMA1_Channel3_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0, 0));
+    NVIC_SetPriority(DMA1_Channel3_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0, 0));// SPI TX
     NVIC_EnableIRQ(DMA1_Channel3_IRQn);
     /* DMA1_Channel4_IRQn interrupt configuration */
-    NVIC_SetPriority(DMA1_Channel4_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0, 0));
+    NVIC_SetPriority(DMA1_Channel4_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0, 0));// USART TX
     NVIC_EnableIRQ(DMA1_Channel4_IRQn);
     /* DMA1_Channel5_IRQn interrupt configuration */
-    NVIC_SetPriority(DMA1_Channel5_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0, 0));
+    NVIC_SetPriority(DMA1_Channel5_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0, 0));// USART RX
     NVIC_EnableIRQ(DMA1_Channel5_IRQn);
     /* DMA1_Channel6_IRQn interrupt configuration */
-    NVIC_SetPriority(DMA1_Channel6_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0, 0));
+    NVIC_SetPriority(DMA1_Channel6_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0, 0));// I2C TX
     NVIC_EnableIRQ(DMA1_Channel6_IRQn);
     /* DMA1_Channel7_IRQn interrupt configuration */
-    NVIC_SetPriority(DMA1_Channel7_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0, 0));
+    NVIC_SetPriority(DMA1_Channel7_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0, 0));// I2C RX
     NVIC_EnableIRQ(DMA1_Channel7_IRQn);
 }
 
